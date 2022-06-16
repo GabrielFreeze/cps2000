ef6a3539a5586b226ad9d3e5c36d90bb18ff7a90
Gabriel_Freeze <gabriel.hili.20@um.edu.mt>
2022-06-16 12:22:52 +0200

    Final Push

--- src/programs/program_03.rs
+++ src/programs/program_03.rs
@@ -1,21 +1,34 @@
-fn nth_fibonacci(n:int) -> int {
-    let a:int = 0;
-    let b:int = 1;
-    let c:int = 0;
+//Function definition for Power
+fn Pow(x:float , n:int) -> float {
+    
+    let y : float = 1.0;
 
-    for (let i:int = 0; i < n; i = i+1) {
-        c = b;
-        b = a+b;
-        a = c;
+    if (n > 0) {
+        for (; n>0; n=n-1) {
+            y = y*x;
+        }
+
+        if (y == 3) {
+            if (x == 2) {
+                return y;
+                x = 2;
+            }
+            else {
+                return x;
+            }
+        } else {
+            return y;
+        }
+        
+    } else {
+        for (; n < 0; n=n+1) {
+            y = y/x;
+        }
+
+        return x;
     }
-
-    return a;
 }
 
-fn return_char() -> char {
-    return 'a';
-}
+let x : float = Pow(2.1,10);
 
-// let i: float = -4;
-
-print 'a' + return_char();
+print x; //prints to console 1667.988
